79d10868ed8f0e0c1ace4bbe5719004e04c07fad
TrieuPhu22 <phuhao00112233@gmail.com>
2025-04-08 11:55:13 +0700

    .

--- src/main/java/ut/edu/pickleball_booking/entity/Booking.java
+++ src/main/java/ut/edu/pickleball_booking/entity/Booking.java
@@ -1,4 +1,69 @@
 package ut.edu.pickleball_booking.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String customerName;
+    private LocalDate bookingDate;
+    private LocalDate startTime;
+    private LocalDate endTime;
+
+    @ManyToOne
+    @JoinColumn(name ="court_id")
+    private Court court;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDate bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public LocalDate getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalDate startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalDate getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDate endTime) {
+        this.endTime = endTime;
+    }
+
+    public Court getCourt() {
+        return court;
+    }
+
+    public void setCourt(Court court) {
+        this.court = court;
+    }
 }
