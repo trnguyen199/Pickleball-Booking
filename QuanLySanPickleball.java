3d1d2442a67114986debf744c27df4b0f8e44e55
sangno1 <psang531@gmail.com>
2025-04-13 23:01:02 +0700

    Add files via upload

---
+++ QuanLySanPickleball.java
@@ -0,0 +1,12 @@
+package com.pickleball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLySanPickleball {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuanLySanPickleball.class, args);
+    }
+}
